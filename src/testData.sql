8e90255487051c40b19e1e53c73b3579b2f41100
tobiasras <tobiasras.job@gmail.com>
2022-12-20 06:00:32 +0100

    created method in orderController

---
+++ src/testData.sql
@@ -0,0 +1,16 @@
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (1, 'Cola', 15, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (2, 'Sprite', 15, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (3, 'Sennep', 30, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (4, 'Tomat', 30, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (5, 'Agurk', 30, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (6, 'Peber', 30, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (7, 'Salt', 30, 500);
+INSERT INTO product_type (id, name, price, weight_gram) VALUES (8, 'Brød', 30, 500);
+
+INSERT INTO product_order (delivery_id) VALUES (null);
+INSERT INTO product_order (delivery_id) VALUES (null);
+
+
+
+
+
